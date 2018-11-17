257d0d201bd5f86047123d9dc4dbee2991c651d5
Lemoncreme <schwalegos@gmail.com>
2018-11-17 15:48:43 -0700

    chronus

---
+++ chronus-interruptus/js/resources.js
@@ -0,0 +1,252 @@
+game.resources = [
+    {
+        "name": "jazz",
+        "type": "audio",
+        "src": "data/bgm/"
+    },
+    {
+        "name": "cling",
+        "type": "audio",
+        "src": "data/sfx/"
+    },
+    {
+        "name": "die",
+        "type": "audio",
+        "src": "data/sfx/"
+    },
+    {
+        "name": "enemykill",
+        "type": "audio",
+        "src": "data/sfx/"
+    },
+    {
+        "name": "jump",
+        "type": "audio",
+        "src": "data/sfx/"
+    },
+    {
+        "name": "walk0001",
+        "type": "image",
+        "src": "data/img/assets/character/walk0001.png"
+    },
+    {
+        "name": "walk0002",
+        "type": "image",
+        "src": "data/img/assets/character/walk0002.png"
+    },
+    {
+        "name": "walk0003",
+        "type": "image",
+        "src": "data/img/assets/character/walk0003.png"
+    },
+    {
+        "name": "walk0004",
+        "type": "image",
+        "src": "data/img/assets/character/walk0004.png"
+    },
+    {
+        "name": "walk0005",
+        "type": "image",
+        "src": "data/img/assets/character/walk0005.png"
+    },
+    {
+        "name": "walk0006",
+        "type": "image",
+        "src": "data/img/assets/character/walk0006.png"
+    },
+    {
+        "name": "walk0007",
+        "type": "image",
+        "src": "data/img/assets/character/walk0007.png"
+    },
+    {
+        "name": "walk0008",
+        "type": "image",
+        "src": "data/img/assets/character/walk0008.png"
+    },
+    {
+        "name": "walk0009",
+        "type": "image",
+        "src": "data/img/assets/character/walk0009.png"
+    },
+    {
+        "name": "walk0010",
+        "type": "image",
+        "src": "data/img/assets/character/walk0010.png"
+    },
+    {
+        "name": "walk0011",
+        "type": "image",
+        "src": "data/img/assets/character/walk0011.png"
+    },
+    {
+        "name": "coin",
+        "type": "image",
+        "src": "data/img/assets/collectables/coin.png"
+    },
+    {
+        "name": "key",
+        "type": "image",
+        "src": "data/img/assets/collectables/key.png"
+    },
+    {
+        "name": "fly_dead",
+        "type": "image",
+        "src": "data/img/assets/enemies/fly_dead.png"
+    },
+    {
+        "name": "fly_fly",
+        "type": "image",
+        "src": "data/img/assets/enemies/fly_fly.png"
+    },
+    {
+        "name": "fly_normal",
+        "type": "image",
+        "src": "data/img/assets/enemies/fly_normal.png"
+    },
+    {
+        "name": "slime_dead",
+        "type": "image",
+        "src": "data/img/assets/enemies/slime_dead.png"
+    },
+    {
+        "name": "slime_normal",
+        "type": "image",
+        "src": "data/img/assets/enemies/slime_normal.png"
+    },
+    {
+        "name": "slime_walk",
+        "type": "image",
+        "src": "data/img/assets/enemies/slime_walk.png"
+    },
+    {
+        "name": "platform",
+        "type": "image",
+        "src": "data/img/assets/hazards/platform.png"
+    },
+    {
+        "name": "weight",
+        "type": "image",
+        "src": "data/img/assets/hazards/weight.png"
+    },
+    {
+        "name": "shadedDark01",
+        "type": "image",
+        "src": "data/img/assets/UI/shadedDark01.png"
+    },
+    {
+        "name": "shadedDark07",
+        "type": "image",
+        "src": "data/img/assets/UI/shadedDark07.png"
+    },
+    {
+        "name": "shadedDark13",
+        "type": "image",
+        "src": "data/img/assets/UI/shadedDark13.png"
+    },
+    {
+        "name": "shadedDark15",
+        "type": "image",
+        "src": "data/img/assets/UI/shadedDark15.png"
+    },
+    {
+        "name": "shadedDark30",
+        "type": "image",
+        "src": "data/img/assets/UI/shadedDark30.png"
+    },
+    {
+        "name": "shadedDark36",
+        "type": "image",
+        "src": "data/img/assets/UI/shadedDark36.png"
+    },
+    {
+        "name": "background",
+        "type": "image",
+        "src": "data/img/background.png"
+    },
+    {
+        "name": "bg_stars",
+        "type": "image",
+        "src": "data/img/bg_stars.png"
+    },
+    {
+        "name": "clouds",
+        "type": "image",
+        "src": "data/img/clouds.png"
+    },
+    {
+        "name": "texture",
+        "type": "image",
+        "src": "data/img/texture.png"
+    },
+    {
+        "name": "tileset",
+        "type": "image",
+        "src": "data/img/tileset.png"
+    },
+    {
+        "name": "texture",
+        "type": "json",
+        "src": "data/img/texture.json"
+    },
+    {
+        "name": "intro",
+        "type": "tmx",
+        "src": "data/map/intro.tmx"
+    },
+    {
+        "name": "level1",
+        "type": "tmx",
+        "src": "data/map/level1.tmx"
+    },
+    {
+        "name": "map0",
+        "type": "tmx",
+        "src": "data/map/map0.tmx"
+    },
+    {
+        "name": "map1",
+        "type": "tmx",
+        "src": "data/map/map1.tmx"
+    },
+    {
+        "name": "template",
+        "type": "tmx",
+        "src": "data/map/template.tmx"
+    },
+    {
+        "name": "template2",
+        "type": "tmx",
+        "src": "data/map/template2.tmx"
+    },
+    {
+        "name": "map1",
+        "type": "tmx",
+        "src": "data/map/map1.json"
+    },
+    {
+        "name": "map2",
+        "type": "tmx",
+        "src": "data/map/map2.json"
+    },
+    {
+        "name": "tileset",
+        "type": "tmx",
+        "src": "data/map/tileset.json"
+    },
+    {
+        "name": "tileset",
+        "type": "tsx",
+        "src": "data/map/tileset.tsx"
+    },
+    {
+        "name": "PressStart2P",
+        "type": "binary",
+        "src": "data/fnt/PressStart2P.fnt"
+    },
+    {
+        "name": "PressStart2P",
+        "type": "image",
+        "src": "data/fnt/PressStart2P.png"
+    }
+];
