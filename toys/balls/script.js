82b49df8ea1628960651731d3aa3100e6d86d75f
benpm <schwalegos@gmail.com>
2019-08-10 17:06:17 -0600

    comments, code cleanup

--- toys/balls/script.js
+++ toys/balls/script.js
@@ -1,7 +1,8 @@
-var canvas = document.getElementById("canvas");
-var ctx = canvas.getContext("2d", {alpha: false});
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d", {alpha: false});
 const $title = $("#title");
 const touch = new Hammer(document.body);
+const ballRate = 0.05;
 
 var balls = [];
 var colors = [
@@ -17,6 +18,7 @@ var clickInterval = null;
 var mx = 0;
 var my = 0;
 
+//Ball class
 function Ball(x, y, vx, vy, size, color, decay) {
     this.x = x;
     this.y = y;
@@ -27,27 +29,30 @@ function Ball(x, y, vx, vy, size, color, decay) {
     this.decay = decay;
 }
 
+//Handler for window resize
 function resizeHandler() {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
 }
 
+//Handler for mouse movement
 function mouseHandler(e) {
     mx = e.clientX;
     my = e.clientY;
 }
 
+//Handler for Hammer touch events
 function touchHandler(e) {
     mx = e.center.x;
     my = e.center.y;
-    if (t - dtball > 0.1) {
-        clickHandler();
+    if (balls.length < 100 && t - dtball > ballRate) {
+        makeBall();
         dtball = t;
     }
 }
 
-function clickHandler() {
-    //if (balls.length > 100) balls.shift();
+//Make a new ball at mouse position
+function makeBall() {
     var radius = Math.random() * 100;
     var ball = new Ball(
         mx - radius / 2.0, my - radius / 2.0,
@@ -56,6 +61,7 @@ function clickHandler() {
     balls.push(ball);
 }
 
+//Draw the specified ball (handler for forEach)
 function drawBall(ball, index, arr) {
     ball.size /= ball.decay;
     if (ball.size < 1.0)
@@ -74,25 +80,33 @@ function drawBall(ball, index, arr) {
     return true;
 }
 
+//Draw loop
 function loop() {
     ctx.fillStyle = "rgba(62,54,57,0.01)";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
     balls = balls.filter(drawBall);
-    $title.text(balls.length);
+    $title.text("Touch Me!");
     t += 1 / 60;
 }
 
-$("body").on("click", clickHandler);
+//Mouse move handler
 $("body").on("mousemove", mouseHandler);
+
+//Touch handlers
 touch.on("pan", touchHandler);
 touch.on("tap", touchHandler);
+
+//Force size canvas
 resizeHandler();
+
+//Initial bg coloring
 ctx.fillStyle = "#3E3639";
 ctx.fillRect(0, 0, canvas.width, canvas.height);
-mx = canvas.width / 2;
-my = canvas.height / 2;
+
+//Initial 
 for (i = 0; i < 10; i++) {
-    clickHandler();
+    mx = Math.random() * canvas.width;
+    my = Math.random() * canvas.height;
+    makeBall();
 }
 setInterval(loop, 1000 / 60);
-//setInterval(clickHandler, 500);
