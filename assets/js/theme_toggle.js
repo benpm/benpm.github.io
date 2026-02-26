4af7e2ec626fbcdf55b6ba9c8ae70fe9ea21d14a
benpm <schwalegos@gmail.com>
2026-02-25 22:20:45 -0700

    refactor: update theme toggle functionality and improve splash text display
    style: enhance about page layout and remove unused interactive content

--- assets/js/theme_toggle.js
+++ assets/js/theme_toggle.js
@@ -1,37 +1,22 @@
 const themeClasses = ["theme--default", "theme--dark"];
-const splashText = [
-    "wasting valuable procrastination time",
-    "learning from someone else's mistakes",
-    "project abandoner extraordiniare",
-    "adventures in aimless tinkering",
-    "spontaneous skullduggery",
-    "procgen goblin"
-];
 const splashNode = document.getElementById("splash-text");
+const splashTarget = "Benjamin Mastripolito";
 var themeIdx = 0;
-var splashIdx = Math.floor(Math.random() * splashText.length);
 var splashTextChar = 0;
-var nextSplashText = "";
 
 function toggleTheme() {
     themeIdx = (themeIdx + 1) % themeClasses.length;
-    splashIdx = (splashIdx + 1) % splashText.length;
     document.body.className = themeClasses[themeIdx];
-    splashTextChar = 1;
-    nextSplashText = `> ${splashText[splashIdx]}`;
-    if (splashNode) {
-        writeSplashText();
-    }
     localStorage.setItem("theme", themeIdx.toString());
 }
 
 function writeSplashText() {
-    splashNode.innerHTML = `${nextSplashText.slice(0, splashTextChar)}|`;
     splashTextChar += 1;
-    if (splashTextChar <= nextSplashText.length) {    
+    if (splashTextChar <= splashTarget.length) {
+        splashNode.innerHTML = `${splashTarget.slice(0, splashTextChar)}|`;
         setTimeout(writeSplashText, 15 - Math.random() * 10 + Math.random() * 20);
     } else {
-        splashNode.innerHTML = nextSplashText;
+        splashNode.innerHTML = splashTarget;
     }
 }
 
@@ -46,3 +31,7 @@ if (localStorage.getItem("theme")) {
 themeIdx = (themeIdx + 1) % themeClasses.length;
 
 toggleTheme();
+
+if (splashNode) {
+    writeSplashText();
+}
--- content/_index.md
+++ content/_index.md
@@ -1,6 +1,10 @@
 ---
-heading: "benpm.github.io"
-subheading: "> wasting valuable procrastination time"
-handle: "Benjamin Mastripolito"
+heading: "Benjamin Mastripolito"
 img: "images/icon.png"
 ---
+
+Hello! My name is Benjamin Mastripolito, and I'm a recent graduate of the University of Utah, where I studied computer graphics under the fantastic guidance of [Dr. Cem Yuksel](https://www.cemyuksel.com/students/#:~:text=Benjamin%20Mastripolito,OpenSpace). My research focused on geometry processing and 3D modeling.
+
+I also love to make games and have a passion for real-time physics and rendering. I have experience with a variety of graphics APIs, including OpenGL, Direct X, Vulkan, and WebGL, and I'm always eager to learn more about the latest advancements in the field. In the past, I've worked on research projects relating to parallel computing and performance analysis.
+
+LinkedIn: [https://www.linkedin.com/in/benjpm/](https://www.linkedin.com/in/benjpm/)
